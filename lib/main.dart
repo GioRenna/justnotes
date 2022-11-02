1119095be66820b69fb4d83a781dec48ce1e1494
Just-Gio <JustG.io@outlook.it>
2022-11-02 07:53:59 +0100

    created login, register and verify_email views

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:justnotes/views/login_view.dart';
+import 'package:justnotes/views/register_view.dart';
 
 import 'firebase_options.dart';
 
@@ -12,6 +13,10 @@ void main() {
       primarySwatch: Colors.blue,
     ),
     home: const HomePage(),
+    routes: {
+      '/login/': (context) => const LoginView(),
+      '/register/': (context) => const RegisterView(),
+    },
   ));
 }
 
@@ -20,29 +25,25 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text('Login'),
-        ),
-        body: FutureBuilder(
-          future: Firebase.initializeApp(
-            options: DefaultFirebaseOptions.currentPlatform,
-          ),
-          builder: (context, snapshot) {
-            switch (snapshot.connectionState) {
-              case ConnectionState.done:
-                final user = FirebaseAuth.instance.currentUser;
-                final emailVerified = user?.emailVerified ?? false;
-                if (emailVerified) {
-                  print('You are a verified user');
-                } else {
-                  print('You need to verify your email first');
-                }
-                return const Text('Done');
-              default:
-                return const Text('Loading...');
-            }
-          },
-        ));
+    return FutureBuilder(
+      future: Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform,
+      ),
+      builder: (context, snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.done:
+            //final user = FirebaseAuth.instance.currentUser;
+            //final emailVerified = user?.emailVerified ?? false;
+            //if (emailVerified) {
+            //  return const Text('Done');
+            //} else {
+            //  return const VerifyEmailView();
+            //}
+            return const LoginView();
+          default:
+            return const CircularProgressIndicator();
+        }
+      },
+    );
   }
 }
