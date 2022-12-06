de7bbfb07273510e3d32bd3065b6d92913ed2b5a
Just-Gio <JustG.io@outlook.it>
2022-12-06 10:45:19 +0100

    step-21

--- lib/views/notes/create_update_note_view.dart
+++ lib/views/notes/create_update_note_view.dart
@@ -3,7 +3,9 @@ import 'package:justnotes/services/auth/auth_service.dart';
 import 'package:justnotes/services/cloud/cloud_note.dart';
 import 'package:justnotes/services/cloud/cloud_storage_exceptions.dart';
 import 'package:justnotes/services/cloud/firebase_cloud_storage.dart';
+import 'package:justnotes/utilities/dialogs/cannot_share_empty_note.dart';
 import 'package:justnotes/utilities/dialogs/generics/get_arguments.dart';
+import 'package:share_plus/share_plus.dart';
 
 class CreateUpdateNote extends StatefulWidget {
   const CreateUpdateNote({super.key});
@@ -90,9 +92,19 @@ class _CreateUpdateNoteState extends State<CreateUpdateNote> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('New Note'),
-      ),
+      appBar: AppBar(title: const Text('New Note'), actions: [
+        IconButton(
+          onPressed: () async {
+            final text = _textController.text;
+            if (_note == null || text.isEmpty) {
+              await showCannotShareEmptyNoteDialog(context);
+            } else {
+              Share.share(text);
+            }
+          },
+          icon: const Icon(Icons.share),
+        ),
+      ]),
       body: FutureBuilder(
         future: createOrGetExistingNote(context),
         builder: (context, snapshot) {
