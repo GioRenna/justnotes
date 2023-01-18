647eab15af807d916fc2afb65534a323c4f731a9
Just-Gio <JustG.io@outlook.it>
2023-01-18 12:25:59 +0100

    step-27

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:justnotes/services/auth/bloc/auth_event.dart';
 import 'package:justnotes/services/auth/bloc/auth_state.dart';
 import 'package:justnotes/services/auth/firebase_auth_provider.dart';
 import 'package:justnotes/services/cloud/cloud_storage_constants.dart';
+import 'package:justnotes/views/forgot_password_view.dart';
 import 'package:justnotes/views/login_view.dart';
 import 'package:justnotes/views/notes/create_update_note_view.dart';
 import 'package:justnotes/views/notes/notes_view.dart';
@@ -52,6 +53,8 @@ class HomePage extends StatelessWidget {
         return const VerifyEmailView();
       } else if (state is AuthStateLoggedOut) {
         return const LoginView();
+      } else if (state is AuthStateForgotPassword) {
+        return const ForgotPasswordView();
       } else if (state is AuthStateRegistering) {
         return const RegisterView();
       } else {
